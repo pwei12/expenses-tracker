6945267e4b90607414f75a25eec2f06bada2efb2
pwei12 <piwei.wong@hackwagon.com>
2021-05-07 19:39:22 +0800

    feat: add header to expenses page

--- pages/expenses.jsx
+++ pages/expenses.jsx
@@ -1,6 +1,30 @@
+import { parse } from 'cookie';
+import MainLayout from '../components/MainLayout';
+import { COOKIE_NAME } from '@/constants/auth';
 
-const ExpensesPage = () => {
-   return <>Placeholder</>
-}
+export const getServerSideProps = context => {
+  const cookies = parse(context.req.headers?.cookie ?? '');
+  const hasLoggedIn = Boolean(cookies[COOKIE_NAME]);
+  return hasLoggedIn
+    ? {
+        props: {
+          hasLoggedIn
+        }
+      }
+    : {
+        redirect: {
+          destination: '/',
+          permanent: false
+        }
+      };
+};
+
+const ExpensesPage = ({ hasLoggedIn }) => {
+  return (
+    <MainLayout hasLoggedIn={hasLoggedIn} hasAuthButton={hasLoggedIn}>
+      Placeholder
+    </MainLayout>
+  );
+};
 
 export default ExpensesPage;
